300425b50251aa00d4a268f5d542310661a4a20e
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-09-21 11:53:31 +0330

    cart page created

---
+++ src/app/cart/[id]/page.tsx
@@ -0,0 +1,11 @@
+import CartPage from '@/template/CartPage'
+import React from 'react'
+
+
+const Cart = () => {
+  return (
+    <CartPage></CartPage>
+  )
+}
+
+export default Cart
--- src/app/menu/action.ts
+++ src/app/menu/action.ts
@@ -1,6 +1,11 @@
+import { category } from '@/types/types';
 import { db } from '@/utils/db';
 
 export const menuAction = async () => {
   const [rows] = await db.query("SELECT * FROM Items");
   return rows;
 };
+export const menuCategoryAction = async (category:category) =>{
+  const [rows] = await db.query("select * from Items where category = ?",[category])
+  return rows
+}
