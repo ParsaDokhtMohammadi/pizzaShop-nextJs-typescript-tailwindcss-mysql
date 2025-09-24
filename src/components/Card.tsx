c3b2edca6c8ff9862d641bd01133bde200048700
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-09-24 12:19:50 +0330

    css styles added to card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,10 +13,13 @@ const Card = async({ data , inCart}: { data: ICartItem , inCart : boolean}) => {
 
     
   return (
-    <div className='flex flex-col p-2 bg-bgPrimary gap-4'>
-        <Image src={data.imageURL || ""} alt={data.name} width={96} height={96}/>
-        <h2>{data.name}</h2>
+    <div className='flex flex-col   gap-4  rounded-2xl  relative items-center'>
+        <Image src={data.imageURL || ""} alt={data.name} width={300} height={300}   className='w-[150px] h-[140px] relative top-20 z-10 '/>
+        <div className='flex flex-col justify-between  h-[200px] w-[225px] rounded-2xl bg-bgPrimary px-5 pb-3 '>
+        <span className='mt-20 text-sm font-medium'>{data.name}</span>
+        <span className='text-sm font-medium'>{data.price} تومان</span>
         {inCart ?<ItemQuantity quantity={data.quantity} />: <AddToCartButton cartId={user_id} itemId={data.id} quantity={1} />}
+        </div>
     </div>
   )
 }
